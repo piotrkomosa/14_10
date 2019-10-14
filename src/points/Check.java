a1b5fe29c466543a8caa857121aa77f0f4f8491b
piotrkomosa <komosa.piotr@gmail.com>
2019-10-14 19:13:18 +0200

    commit3

--- src/points/Check.java
+++ src/points/Check.java
@@ -4,28 +4,30 @@ import java.util.Scanner;
 public class Check {
     public static void main(String[] args) {
     }
-    void String (int x, int y) {
+
+    Point point = new Point();
+    int quaterCheck (int x, int y) {
         if (x > 0 && y > 0) {
-            System.out.println("I quater");
+            System.out.println("I ");
         } else if (x < 0 && y > 0) {
-            System.out.println("II quater");
+            System.out.println("II ");
         } else if (x < 0 && y < 0) {
-            System.out.println("III quater");
-        } else if (x < 0 && y < 0) {
-            System.out.println("IV quater");
+            System.out.println("III ");
+        } else  {
+            System.out.println("IV ");
         }
 
     }
+
     Scanner scanner = new Scanner(System.in);
-    Point point1 = new Point();
     System.out.println("podaj liczbe X: ");
     int x = scanner.nextInt();
     scanner.nextLine();
     System.out.println("podaj liczbe Y: ");
-    int secondNumber = scanner.nextInt();
+    int y = scanner.nextInt();
     scanner.nextLine();
     scanner.close();
-    System.out.println("wynik to: " + point1.check(x, y));
+    System.out.println("Punkt leży na: " + point.quaterCheck() +" cwiartka");
 
 
 }
